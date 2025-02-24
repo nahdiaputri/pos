bda48283580209a5983311b1608e80c920ec8193
Nahdia Putri <nptrsfr1808@gmail.com>
2025-02-24 12:26:05 +0700

    Praktikum4

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
-// Halaman Home
-Route::get('/', [HomeController::class, 'index']);
-
-// Halaman Products dengan prefix category
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function(){
+    return view('welcome');
 });
 
-// Halaman User dengan parameter ID dan Nama
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-// Halaman Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
-
+Route::get('/level', [LevelController::class, 'index']);
