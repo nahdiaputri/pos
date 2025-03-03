6edbff3b1ae0e1010aea2abb13002eeab249e97a
Nahdia Putri <nptrsfr1808@gmail.com>
2025-03-03 12:54:05 +0700

    JS4_Perubahan2_Praktikum2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,8 +57,7 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::with('level')->get();
-        dd($user);
-
+        return view('user', ['data'=> $user]);
     }
 
 }
