ced7b3e802ed66e93f2ca95e44244beedfc2d43f
Nahdia Putri <nptrsfr1808@gmail.com>
2025-03-03 12:46:14 +0700

    JS4_Perubahan1_Praktikum2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
 
     public function tambah()
     {
@@ -53,6 +53,12 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        dd($user);
+
     }
 
 }
---
+++ app/Models/LevelModel.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model
+{
+    protected $table = "m_level";
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class);
+    }
+}
