87a07a9e56fc6457726e84770eae7d9e7665f665
Nahdia Putri <nptrsfr1808@gmail.com>
2025-04-28 16:38:47 +0700

    JS10_PRAKTIKUM4

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\LevelController as ControllersLevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +21,17 @@ use App\Http\Controllers\Api\RegisterController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::get('levels',[LevelController::class,'index']);
+Route::post('levels',[LevelController::class,'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+//----------------------------------------------------------------------------------------------------------------//
 Route::post('/register', \App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', \App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('levels',[LevelController::class,'index']);
